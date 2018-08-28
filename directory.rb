7a762ba94cddceb930c1add01f5b02e0ec676175
Daniel Stephenson <dan_0502@hotmail.co.uk>
2018-08-28 10:51:18 +0100

    Students and student total

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# Print list of students
 puts "The students of Villains Academy"
 puts "----------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,9 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+print "\n"
+#Print total number of students
+print "Overall, we have "
+#It's important that print() doesn't add new line characters
+print 11
+puts " great students"
