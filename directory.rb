4932b3a6c594fadeb9683c771c3eddb47369a5b8
Daniel Stephenson <dan_0502@hotmail.co.uk>
2018-08-31 21:10:22 +0100

    Centred

--- directory.rb
+++ directory.rb
@@ -12,18 +12,18 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "--------------------------------"
+  puts "The students of Villains Academy".center(80)
+  puts "--------------------------------".center(80)
 end
 
 def print(students)
   students.map.with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) (Country of Birth: #{student [:country_of_birth]})"
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) (Country of Birth: #{student [:country_of_birth]})".center(80)
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(80)
 end
 
 students = input_students
